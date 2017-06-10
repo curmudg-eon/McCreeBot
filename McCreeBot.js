f5007689ae78e949aae7984cf4b54e98e3689258
gabeSecula <gabe.secula@gmail.com>
2017-06-10 10:17:09 -0400

    Laid out the basic structure of the program.

--- McCreeBot.js
+++ McCreeBot.js
@@ -1,30 +1,57 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '~';
-//const dispatcher = connection.playFile('mnt/c/Users/Gabe/Downloads/highNoon.mp3');
+
 
 client.login('MzIyNDM4MzcxODQ4ODgwMTMw.DByWrQ.vZb-9j5zfKszPU4clvOrCfpHSmw');
+
+function highNoon (msg) {
+  if ((msg.member.voiceChannel)&&(msg.member.hasPermission('MANAGE_CHANNELS', 'checkAdmin'))){
+    msg.member.voiceChannel.join()
+      .then(connection => {
+        const dispatcher = connection.playFile('./highNoon.mp3');
+        msg.reply('Uh huh, you know what time it is.');
+    })
+    .catch(console.log);
+
+
+  } else if (msg.member.voiceChannel) {
+      msg.reply('Who\'re you?')
+  } else {
+      msg.reply('I can\'t tell you the time if you don\'t let me.');
+  }
+}
+
+function roulette(msg) {
+
+}
+
+function flashbang(msg) {
+
+}
+
+function commands(msg) {
+
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
+
   if (!message.guild) return;
 
-  if (message.content === prefix + 'time') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-          //connection.playFile();
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
+  if (message.content === prefix + 'time'){
+    highNoon(message);} //Deafen or afk all members in a voiceChannel
+
+  if (message.content === prefix + 'spin') {
+    roulette(message);} //Deafen a single random member in the voiceChannel
+
+  if (message.content === prefix + 'flashbang') {
+    flashbang(message);} //Mute
+
+  if ((message.content === prefix + 'help') || (message.content === prefix + 'commands')) {
+    commands(message);} //Direct message a list of commands
+
 });
