9db36f18fd6ed0a9fb552787669ede4728e831ca
Gabriel Secula <gabe.secula@gmail.com>
2017-11-30 12:58:22 -0500

    Update McCreeBot.js
    My oh my, my code is garbage, my. Just cleaned some stuff up.

--- McCreeBot.js
+++ McCreeBot.js
@@ -7,9 +7,10 @@ const prefix = '~';
 client.login('MzIyNDM4MzcxODQ4ODgwMTMw.DByWrQ.vZb-9j5zfKszPU4clvOrCfpHSmw');
 
 function permissionCheck (msg) {
-  if ((msg.member.voiceChannel)&&(msg.member.hasPermission('MANAGE_CHANNELS', 'checkAdmin'))) {
+  if ((msg.member.voiceChannel)&&(msg.member.hasPermission('MANAGE_CHANNELS', 'checkAdmin'))) 
     return true;
-  } else {return false;}
+  else 
+    return false;
 }
 
 function getRandomInt(min, max) {
@@ -22,10 +23,11 @@ function highNoon (msg) {
   msg.member.voiceChannel.join()
     .then(connection => {
       const dispatcher = connection.playFile('./highNoon.mp3');
-      msg.reply('Uh huh, you know what time it is.');
 
       dispatcher.on('end', () => {
         msg.member.voiceChannel.leave();
+        msg.reply('Uh huh, you know what time it is.'); //I should really learn to make it wait.
+        
         var targets = msg.member.voiceChannel.members.array();
         for (var i = 0, len = targets.length; i < len; i++) {
           targets[i].setMute(true);
@@ -83,9 +85,8 @@ function flashbang(msg) {
       dispatcher.on('end', () => {
         msg.member.voiceChannel.leave();
       });
-      for (var i = 0, len = targets.length; i < len; i++) {
+      for (var i = 0, len = targets.length; i < len; i++)
         targets[i].setMute(true);
-      }
   });
 }
 
@@ -102,27 +103,25 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-  if ((message.content === prefix + 'help') || (message.content === prefix + 'commands')) {
-    commands(message);}
+  if ((message.content === prefix + 'help') || (message.content === prefix + 'commands'))
+    commands(message);
 
   if (!message.guild) return;
 
-  if (message.content === prefix + 'revive') {
+  if (message.content === prefix + 'revive')
     revive(message);
-  }
 
   if (permissionCheck(message) === true) {
-  if (message.content === prefix + 'time'){
-    highNoon(message);} //Deafen all members in a voiceChannel
+    if (message.content === prefix + 'time')
+      highNoon(message); //Deafen all members in a voiceChannel
 
-  if (message.content === prefix + 'revive all') {
-    reviveAll(message);
-  }
+    if (message.content === prefix + 'revive all') 
+      reviveAll(message);
 
-  if (message.content === prefix + 'spin') {
-    roulette(message);} //Deafen a single random member in the voiceChannel
+    if (message.content === prefix + 'spin')
+      roulette(message); //Deafen a single random member in the voiceChannel
 
-  if (message.content === prefix + 'flashbang') {
-    flashbang(message);} //Mute
+    if (message.content === prefix + 'flashbang')
+      flashbang(message); //Mute
   }
 });
